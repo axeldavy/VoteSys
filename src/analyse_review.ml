cf45445497f020e8ca373f465f5a5c29ce46c90b
MargotCalbrix <margot.calbrix@gmail.com>
2014-01-13 15:05:29 +0100

    nombre de votes min

--- src/analyse_review.ml
+++ src/analyse_review.ml
@@ -1,4 +1,5 @@
 open Ast 
+open List
 
 let attribute_coeffs methode product_list user_hashtable =
 (* retourne une liste de (float list)* product *)
@@ -11,3 +12,22 @@ let attribute_coeffs methode product_list user_hashtable =
    | Helpfullness -> failwith "TODO"
    | Mode -> failwith "TODO"
    | Anciennete -> failwith "TODO"
+
+
+let comp_nbreviews prod1 prod2 =
+  let Product(_, rev1) = prod1 and Product(_, rev2) = prod2 in
+  let l1 = List.length rev1 and l2 = List.length rev2 in
+  if l1 > l2 then 1
+  else if l1 < l2 then -1
+      else 0;;
+
+let min_reviews_quartile product_list =
+  List.sort comp_nbreviews product_list;
+  let p = List.nth product_list ((List.length product_list) / 4) in
+  let Product(_, rev) = p in
+  List.length rev;;
+
+let define_min_reviews methode product_list =
+  match methode with
+  | Quartile -> min_reviews_quartile product_list
+  | _ -> failwith "TODO"
