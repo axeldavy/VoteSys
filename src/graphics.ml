0b8c70310bca591526999b99e6013d8c2ffc552b
Axel Davy <axel.davy@ens.fr>
2014-01-14 11:25:37 +0100

    module gnuplot ok

--- src/graphics.ml
+++ src/graphics.ml
@@ -1,9 +1,11 @@
-(*import Gnuplot
+open Array
+open Ast
+(*open Gnuplot*)
 
 let draw_distribution x y = 
-   let gphandle = Gnuplot.init (EPS("./test.ps")) in
-   close gphandle
-   
+   let gphandle = Gnuplot.init (Gnuplot.EPS("./test.ps")) in
+   Gnuplot.close gphandle
+
    
 let draw_distribution_2 review_list =
    let x = [|1;2;3;4;5 |] in
@@ -11,12 +13,13 @@ let draw_distribution_2 review_list =
    let n = Array.make 5 0 in
    let fill_n review =
       let Review_score (score) = review.review_score in
-      let iscore = int_of_float (score + 0.5) -1 in
-      n(iscore) = n(iscore)+1;
+      let iscore = int_of_float (score +. 0.5) -1 in
+      n.(iscore) = n.(iscore)+1;
+      ()
    in
    List.iter fill_n review_list;
-   let sumn = n(0) + n(1) + n(2) + n(3) + n(4) in
+   let sumn = n.(0) + n.(1) + n.(2) + n.(3) + n.(4) in
    for i = 0 to 4 do
-      y(i) = float_of_int(n(i)) /. float_of_int(sumn);
-   done
-   draw_distribution x y*)
+      y.(i) = float_of_int(n.(i)) /. float_of_int(sumn);
+   done;
+   draw_distribution x y
