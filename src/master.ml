f04d2b26f3aeb9bc22c970350f7a3611e59d092c
Axel Davy <axel.davy@ens.fr>
2014-01-20 13:44:45 +0100

    Plus d'info

--- src/master.ml
+++ src/master.ml
@@ -16,22 +16,16 @@ let print_classement classement =
    in
    let mean = (List.fold_right calc_mean classement 0.) /. (float_of_int (List.length(classement))) in
    Format.printf "mean products notes: %0.3f@." mean;
-   for i = 0 to 10 do
+   for i = 0 to 9 do
       let (Product (pr,rl),score) = List.nth classement i in
       let p = List.hd(rl) in
       let Product_id(product_id) = p.product_id in
       let Product_title(product_title) = p.product_title in
-      Format.printf "%d: mean %f, Product %s:%s @." i score product_id product_title;
+      Format.printf "%d: mean %f, Product %s:%s @." (i+1) score product_id product_title;
    done;
    ()
 
-let process review_list =
-   let product_list = Util.get_product_list_of_review_list review_list in
-   let userhashtbl = Util.get_user_hashtbl review_list in
-   let entry_list = Analyse_review.attribute_coeffs Simple product_list userhashtbl in
-   if (verbose) 
-   then Format.printf "Coeffs: Simple @.";
-   
+let process_2 review_list entry_list product_list fname =
    let naif = triNaif entry_list in
    if (verbose)
    then print_classement naif;
@@ -49,4 +43,28 @@ let process review_list =
    then print_classement imdbmean;
    
    plot_distribution_notes review_list;
-   plot_distributions "means_simple" [naif; imdbquartile] ["Naif";"IMDBQuartile"]
+   plot_distributions fname [naif; imdbquartile] ["Naif";"IMDBQuartile"]
+   
+let process review_list =
+   let product_list = Util.get_product_list_of_review_list review_list in
+   let userhashtbl = Util.get_user_hashtbl review_list in
+   let entry_list_simple = Analyse_review.attribute_coeffs Simple product_list userhashtbl in
+   if (verbose) 
+   then Format.printf "Coeffs: Simple @.";
+   process_2 review_list entry_list_simple product_list "means_simple";
+   
+   let entry_list_mode = Analyse_review.attribute_coeffs Mode product_list userhashtbl in
+   if (verbose) 
+   then Format.printf "@.Coeffs: Mode @.";
+   process_2 review_list entry_list_mode product_list "means_mode";
+   
+   let entry_list_helpfullness = Analyse_review.attribute_coeffs Helpfullness product_list userhashtbl in
+   if (verbose) 
+   then Format.printf "@.Coeffs: Helpfullness @.";
+   process_2 review_list entry_list_helpfullness product_list "means_helpfullness";
+   
+   let entry_list_anciennete = Analyse_review.attribute_coeffs Anciennete product_list userhashtbl in
+   if (verbose) 
+   then Format.printf "@.Coeffs: Anciennete @.";
+   process_2 review_list entry_list_anciennete product_list "means_anciennete";
+
